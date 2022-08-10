2599d6d75e682da03c215ec531fd0d6893f6d3d6
Dipesh Ss <dipeshshivrame@gmail.com>
2022-08-11 00:31:34 +0530

    social icons on large screen and mobile navigation bug fixes

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
-function updateList() {
-    const titles = [...document.querySelectorAll('h1, h2')].sort((a, b) => {
-        return Math.abs(a.getBoundingClientRect().top) - Math.abs(b.getBoundingClientRect().top)
-    })
-
-    document.querySelectorAll('.selected-circle').forEach(c => c.classList.remove('selected-circle'))
-
-    document.querySelectorAll('.nav-dot')[[...document.querySelectorAll('h1, h2')].indexOf(titles[0])].classList.add('selected-circle')
-}
-
-window.addEventListener('scroll', updateList);
-
 const navbar = document.querySelector('.nnav')
 const hamburger = document.querySelector('.hamburger')
 const icon = document.querySelectorAll('.mobile-nav-icon')
@@ -19,4 +7,6 @@ hamburger.addEventListener('click', () => {
     navbar.classList.toggle('translate-x-full')
     icon[0].classList.toggle('hidden')
     icon[1].classList.toggle('hidden')
+    hamburger.classList.toggle('absolute')
+    hamburger.classList.toggle('fixed')
 })
